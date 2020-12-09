a4564dc290ccc84a8b80da687ad087ea07101b26
tack-k <tyool9c@gmail.com>
2020-12-09 23:35:03 +0900

    カレンダーコンポーネント化

---
+++ src/components/Calender.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+const Calender = (props) => {
+const {selectedDate, handleDateChange} = props;
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="登録したい日付を選択"
+          format="MM/dd/yyyy"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+    </MuiPickersUtilsProvider>
+  )
+}
+
+export default Calender
