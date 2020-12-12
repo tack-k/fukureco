2e3a4f6e554441dba8f115f4b92dad07ba791cd3
tack-k <tyool9c@gmail.com>
2020-12-12 18:05:44 +0900

    MenuBar変数移動

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Menu from '@material-ui/core/Menu';
 import IconButton from '@material-ui/core/IconButton';
 import MenuItem from '@material-ui/core/MenuItem';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 
-const MenuBar = (props) => {
-const {handleClick, handleClose, anchorEl, open} = props;
+const MenuBar = () => {
+const [anchorEl, setAnchorEl] = useState(null);
 
 const options = [
   '変更',
   '削除',
 ];
+
+
+const open = Boolean(anchorEl);
+
+const handleClick = (event) => {
+  setAnchorEl(event.currentTarget);
+};
+
+const handleClose = () => {
+  setAnchorEl(null);
+};
+
+
+
 const ITEM_HEIGHT = 48;
 
   return (
